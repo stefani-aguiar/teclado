08b43d55786ff2fac78a95a215b9123480c6caa9
stefani <stefani99aguiar@gmail.com>
2022-12-12 11:21:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(' .tecla');
+
+
+let contador = 0;
+
+for ( let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const IdAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom (IdAudio);
+    
+    }
+    
+
+}
+
