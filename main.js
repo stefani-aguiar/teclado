e6a01ff3e1b918cd868de4304d7db51fcb853fce
stefani <stefani99aguiar@gmail.com>
2022-12-19 15:13:05 -0300

    alteraçao toca Som

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorElemento) {
+
+    const elemento = document.querySelector(seletorElemento);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+
+    else {
+        console.log('elemento ou seletor não encontrado!')
+    }
 }
 
-const listaDeTeclas = document.querySelectorAll(' .tecla');
+const listaDeTeclas = document.querySelector('.tecla')
 
-
-
-for ( let contador = 0; contador < listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-
     const IdAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
-        tocaSom (IdAudio);
-    
+        tocaSom(IdAudio);
+
     }
-    
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
 
 }
 
